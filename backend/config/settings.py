af9153c14b828a94f4efd1a393abfc24569b2aac
shr3y4w <shreya.sameer04@gmail.com>
2025-01-30 21:01:00 +0530

    added accounts to settings, postgresql to gitignore and register.html some imp changes to connect form to backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', # Add this
+    'accounts',
 ]
 
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'config.urls'
 
